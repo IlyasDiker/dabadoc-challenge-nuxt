665ae9a6dc2de73f13b9568b73def9a6e8a5b5e6
IlyasDiker <ilyasbenhssine@gmail.com>
2023-01-29 14:02:09 +0100

    add Navbar + IconApp + AskQuestion Components

---
+++ components/AskQuestion.vue
@@ -0,0 +1,9 @@
+<template>
+    <section >
+        <form>
+            <div class="grid grid-cols-4">
+                .input
+            </div>
+        </form>
+    </section>
+</template>
---
+++ components/NavbarUi.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="bg-gray-900 text-gray-50">
+        <div class="container mx-auto py-3 px-4">
+            <div class="flex flex-row justify-between items-center">
+                <NuxtLink to="/"><IconApp class="text-white"/></NuxtLink>
+                <div class="flex flex-row items-center gap-5">
+                    <nuxt-link to="login">Login</nuxt-link>
+                    <nuxt-link to="register">Register</nuxt-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
